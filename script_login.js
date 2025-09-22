f84f64b18a0f20e1a7cba57814a2f729cfd84b44
lNataly8a <lnatalyochoa8@gmail.com>
2025-09-22 15:25:39 -0500

    Deploy Front

--- script_login.js
+++ script_login.js
@@ -114,7 +114,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
 
       try {
-        const response = await fetch("https://main.drkoft4my5rgd.amplifyapp.com/api/usuarios/login", {
+        const response = await fetch("http://localhost:8080/api/usuarios/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ correo: email, password })
